a4648af0fa18b90bb03f72afc8f111d6fa520994
Nestor Desabille <nestordesabille4@gmail.com>
2023-07-03 13:23:08 +0800

    create models

--- backend/reservation/models.py
+++ backend/reservation/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+import uuid
 
 
 class Room(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=20)
     description = models.CharField(max_length=500)
+    price = models.FloatField(default=0)
+    type = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+class Bookings(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer_name = models.CharField(max_length=200)
+    room_id = models.ForeignKey("Room", on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
