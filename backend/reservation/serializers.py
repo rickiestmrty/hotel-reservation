b20443c1b4713f825095012d678e33eaeeddb37c
Nestor Desabille <nestordesabille4@gmail.com>
2023-07-05 19:17:16 +0800

    separate time and date returns

--- backend/reservation/serializers.py
+++ backend/reservation/serializers.py
@@ -17,9 +17,11 @@ class BookingSerializer(serializers.ModelSerializer):
         representation["room_id"] = instance.room_id.id
         representation["room_name"] = instance.room_id.name
         representation["start_date"] = instance.start_date
-        representation["start_date_string"] = instance.start_date.strftime('%B %d %Y - %H:%M')
+        representation["start_date_string"] = instance.start_date.strftime('%B %d %Y')
+        representation["start_time_string"] = instance.start_date.strftime('%H:%M')
         representation["end_date"] = instance.end_date
-        representation["end_date_string"] = instance.end_date.strftime('%B %d %Y - %H:%M')
+        representation["end_date_string"] = instance.end_date.strftime('%B %d %Y')
+        representation["end_time_string"] = instance.end_date.strftime('%H:%M')
         representation["created_at"] = instance.created_at
         representation["modified_at"] = instance.modified_at
 
